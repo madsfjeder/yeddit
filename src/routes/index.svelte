b13fcc37609505828ed8d9eccf98b1a889228d33
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-03-27 17:34:26 +0200

    Token wip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import SearchInput from "../components/ui/SearchInput.svelte";
 	import {token} from "../stores/stores";
-
+	const redirectUrl = 'http://localhost:3000/oauth';
+	const authUrl = `https://www.reddit.com/api/v1/authorize?client_id=Bt0zJiirFQI3lGtqPM-W5A&response_type=code&state=2&redirect_uri=${redirectUrl}&duration=permanent&scope=read`;
 	let t;
 	token.subscribe(token => {
 		t = token;
@@ -9,6 +10,12 @@
 </script>
 
 <div class="p-52">
-	<SearchInput/>
-	<p>{t ?? ""}</p>
+	{#if t == null}
+		<div>
+			<a href={authUrl}>Auth here!</a>
+		</div>
+	{:else}
+		<SearchInput/>
+		<p>{t ?? ""}</p>
+	{/if}
 </div>
