8458359d1878a7fc2a91cbbd2e61ba8e0f0ac709
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-04-30 21:08:57 +0200

    wip

--- src/components/ui/VideoArea.svelte
+++ src/components/ui/VideoArea.svelte
@@ -3,65 +3,85 @@
 	import { ArrowSmLeft, ArrowSmRight } from '@steeze-ui/heroicons';
 	import YouTubeCustom from './YouTubeCustom.svelte';
 	import type { VideoLink } from '../../helpers/generateVideoList';
+	import VideoPreview from './VideoPreview.svelte';
 	export let links: VideoLink[];
 	let currentVideoIndex = 0;
 	$: currentVideo = links[currentVideoIndex];
 	let player;
 	const onReady = (event) => {
-		if (event != null && player != null) {
+		/*if (event != null && player != null) {
 			player.playVideo();
-		}
+		}*/
 	};
 </script>
 
-<div class="mt-10 flex h-[26rem] w-full flex-row justify-center align-middle">
-	<button
-		class="mr-4 h-14"
-		on:click={() => {
-			currentVideoIndex--;
-		}}
-	>
-		<Icon src={ArrowSmLeft} class="fill-blue-700" />
-	</button>
-	<div class="flex-col space-y-3">
-		<h1 class="font-bold">
-			{currentVideo.title}
-		</h1>
-		<div class="flex justify-between">
-			<p>
-				Posted by {currentVideo.author}
-			</p>
-			<a
-				class="text-blue-500 underline"
-				href={'https://www.reddit.com' + currentVideo.permalink}
-			>
-				Original post
-			</a>
+<div class="mt-10 flex h-[26rem] w-full flex-row justify-between">
+	<div class="flex flex-row align-middle">
+		<div class="flex-col space-y-3">
+			<div class="flex flex-row justify-between">
+				<button
+					class="mr-4 h-14"
+					on:click={() => {
+						currentVideoIndex--;
+					}}
+				>
+					<Icon src={ArrowSmLeft} class="fill-blue-700" />
+				</button>
+				<div>
+					<h1 class="font-bold">
+						{currentVideo.title}
+					</h1>
+					<div class="flex justify-between">
+						<p>
+							Posted by {currentVideo.author}
+						</p>
+						<a
+							class="text-blue-500 underline"
+							href={'https://www.reddit.com' +
+								currentVideo.permalink}
+						>
+							Original post
+						</a>
+					</div>
+				</div>
+				<button
+					class="ml-4 h-14"
+					on:click={() => {
+						currentVideoIndex++;
+					}}
+				>
+					<Icon src={ArrowSmRight} class="color-blue-700" />
+				</button>
+			</div>
+
+			<YouTubeCustom
+				bind:player
+				on:end={() => currentVideoIndex++}
+				on:ready={onReady}
+				videoId={currentVideo.id}
+				id={1}
+				class={''}
+				options={{
+					height: '390',
+					width: '780',
+					playerVars: {
+						autoplay: 0
+					}
+				}}
+			/>
 		</div>
-
-		<YouTubeCustom
-			bind:player
-			on:end={() => currentVideoIndex++}
-			on:ready={onReady}
-			videoId={currentVideo.id}
-			id={1}
-			class={''}
-			options={{
-				height: '390',
-				width: '640',
-				playerVars: {
-					autoplay: 1
-				}
-			}}
-		/>
 	</div>
-
-	<button
-		class="ml-4 h-14"
-		on:click={() => {
-			currentVideoIndex++;
-		}}
-	>
-		<Icon src={ArrowSmRight} class="color-blue-700" />
-	</button>
+	<div class="h-[36rem] w-1/4 overflow-y-scroll">
+		{#each links as video, index}
+			<VideoPreview
+				subName={video.subreddit}
+				{index}
+				playClicked={(index) => {
+					currentVideoIndex = index;
+				}}
+				title={video.title}
+				thumbnailUrl={video.thumbnail}
+			/>
+		{/each}
+	</div>
 </div>
---
+++ src/components/ui/VideoPreview.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let duration;
+	export let subName;
+	export let thumbnailUrl;
+	export let index;
+	export let title;
+	export let playClicked;
+</script>
+
+<div
+	on:click={() => playClicked(index)}
+	class="m-2 flex w-4/5 cursor-pointer flex-col justify-center space-y-1 rounded-md p-4 shadow-lg"
+>
+	<p class="text-sm font-bold">{subName}</p>
+	<p class="text-sm">{title}</p>
+	<div class="flex w-full ">
+		<img width="168" height="126" src={thumbnailUrl} />
+	</div>
+</div>
