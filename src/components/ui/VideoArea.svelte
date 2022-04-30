784b90792b6cd903438b8ad47f58487e83e1b1ab
madsfjeder <43757066+madsfjeder@users.noreply.github.com>
2022-04-30 15:59:36 +0200

    Added metadata + fields

--- src/components/ui/VideoArea.svelte
+++ src/components/ui/VideoArea.svelte
@@ -1,9 +1,11 @@
-<script>
+<script lang="ts">
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { ArrowSmLeft, ArrowSmRight } from '@steeze-ui/heroicons';
 	import YouTubeCustom from './YouTubeCustom.svelte';
-	export let links;
+	import type { VideoLink } from '../../helpers/generateVideoList';
+	export let links: VideoLink[];
 	let currentVideoIndex = 0;
+	$: currentVideo = links[currentVideoIndex];
 	let player;
 	const onReady = (event) => {
 		if (event != null && player != null) {
@@ -21,22 +23,38 @@
 	>
 		<Icon src={ArrowSmLeft} class="fill-blue-700" />
 	</button>
+	<div class="flex-col space-y-3">
+		<h1 class="font-bold">
+			{currentVideo.title}
+		</h1>
+		<div class="flex justify-between">
+			<p>
+				Posted by {currentVideo.author}
+			</p>
+			<a
+				class="text-blue-500 underline"
+				href={'https://www.reddit.com' + currentVideo.permalink}
+			>
+				Original post
+			</a>
+		</div>
 
-	<YouTubeCustom
-		bind:player
-		on:end={() => currentVideoIndex++}
-		on:ready={onReady}
-		videoId={links[currentVideoIndex]}
-		id={1}
-		class={''}
-		options={{
-			height: '390',
-			width: '640',
-			playerVars: {
-				autoplay: 1
-			}
-		}}
-	/>
+		<YouTubeCustom
+			bind:player
+			on:end={() => currentVideoIndex++}
+			on:ready={onReady}
+			videoId={currentVideo.id}
+			id={1}
+			class={''}
+			options={{
+				height: '390',
+				width: '640',
+				playerVars: {
+					autoplay: 1
+				}
+			}}
+		/>
+	</div>
 
 	<button
 		class="ml-4 h-14"
